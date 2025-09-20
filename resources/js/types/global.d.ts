bd3511cf7b845a18198d8424fa88f718acb36cbd
rv-bit <robertvilcu89@gmail.com>
2025-09-20 19:18:32 +0100

    fix: typescript throwing a tantrum, added some global types

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -3,9 +3,14 @@ import { AxiosInstance } from "axios";
 import { route as ziggyRoute } from "ziggy-js";
 import { PageProps as AppPageProps } from ".";
 
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
 declare global {
 	interface Window {
 		axios: AxiosInstance;
+		Echo: Echo;
+		Pusher: typeof Pusher;
 		showOpenFilePicker?: (options?: { multiple?: boolean; types?: { description: string; accept: Record<string, string[]> }[] }) => Promise<FileSystemFileHandle[]>;
 	}
 
