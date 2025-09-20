ddfdc1edbebc96bd7ee5371e11122912c9b635ee
rv-bit <robertvilcu89@gmail.com>
2025-09-20 19:18:59 +0100

    chore: add socket id to headers when using Axios

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+var socketId = window.Echo.socketId();
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common["X-Socket-ID"] = socketId;
+
+window.axios.defaults.withCredentials = true;
