af4ac5a4bd5448d5597ee9a9e7f68b7cfa4a1340
rv-bit <robertvilcu89@gmail.com>
2025-09-23 21:02:13 +0100

    fix: issue with socket id not getting attached properly

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,9 +1,17 @@
+import { configureEcho } from "@laravel/echo-react";
 import axios from "axios";
+
 window.axios = axios;
 
-var socketId = window.Echo.socketId();
+configureEcho({
+	broadcaster: "reverb",
+	key: import.meta.env.VITE_REVERB_APP_KEY,
+	wsHost: import.meta.env.VITE_REVERB_HOST,
+	wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+	wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+	forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+	enabledTransports: ["ws", "wss"],
+});
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.headers.common["X-Socket-ID"] = socketId;
-
 window.axios.defaults.withCredentials = true;
