f641800386f90e81e8a3cddb7bfcc117ffbfb53d
rv-bit <robertvilcu89@gmail.com>
2025-09-24 22:16:39 +0100

    chore: refactor redundant code

--- resources/js/Components/sidebar/header.tsx
+++ resources/js/Components/sidebar/header.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { APP_NAME } from "@/resources/app-config";
 
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
@@ -10,9 +12,68 @@ import { ChevronDown } from "lucide-react";
 
 import NavLink from "../navigation-link";
 
-export default function Header() {
+export const QuickActions = React.forwardRef<HTMLDivElement>((props, ref) => {
 	const { toggleSidebar } = useSidebar();
 
+	return (
+		<>
+			<SidebarMenuButton
+				onClick={(e) => {
+					toggleSidebar();
+				}}
+				data-sidebar="trigger"
+				data-slot="sidebar-trigger"
+				tooltip={{ size: "sm", content: "Hide sidebar", side: "bottom" }}
+				isTooltipHidden={false}
+				className="group/sidebar-trigger hover:bg-sidebar-accent flex size-fit items-center justify-center overflow-hidden rounded-sm p-2 py-2.5 text-white opacity-100 transition-opacity duration-200 ease-in-out group-data-[collapsible=icon]:w-full group-data-[collapsible=icon]:rounded-md group-data-[collapsible=icon]:bg-transparent group-data-[collapsible=icon]:p-3 [&>svg]:size-auto"
+			>
+				<SidebarTriggerIcon width={15} height={15} stroke="white" className="right-10 rotate-180 rounded-xs bg-black/50 fill-none px-[1px]" />
+			</SidebarMenuButton>
+
+			<div className="hover:bg-sidebar-accent/65 flex h-9 w-fit items-center justify-center overflow-hidden rounded-xl">
+				<SidebarMenuButton
+					data-sidebar="create-chat"
+					data-slot="create-chat"
+					aria-label="Start New Chat"
+					tooltip={{ size: "sm", content: "Start New Chat", side: "bottom" }}
+					isTooltipHidden={false}
+					className="hover:bg-sidebar-accent flex size-full w-8 items-center justify-center py-2 text-white opacity-100 transition-opacity duration-200 ease-in-out [&>svg]:size-auto"
+				>
+					<PencilSquare width={25} height={25} className="fill-white stroke-black/50" />
+				</SidebarMenuButton>
+
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<SidebarMenuButton
+							data-sidebar="chat-dropdown"
+							data-slot="chat-dropdown"
+							aria-label="Actions Menu"
+							title="Open Actions Menu"
+							isTooltipHidden={true}
+							className="hover:bg-sidebar-accent flex size-full w-[1.5rem] items-center justify-center border-none py-2 text-white opacity-100 transition-opacity duration-200 ease-in-out focus-visible:ring-0 [&>svg]:size-auto"
+						>
+							<ChevronDown width={20} height={20} className="stroke-black/50" />
+						</SidebarMenuButton>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="start" side="bottom" className="border-none bg-white">
+						<DropdownMenuItem asChild className="hover:bg-sidebar-accent/50 cursor-pointer bg-transparent select-auto hover:text-black focus:bg-transparent">
+							<Button
+								aria-label="Create new conversation"
+								title="Create new conversation"
+								isTooltipHidden={true}
+								className="focus:bg-sidebar-accent/50 active:bg-sidebar-accent/50 relative flex items-center justify-start py-2 text-black opacity-100 shadow-none transition-opacity duration-200 ease-in-out focus:text-black focus-visible:ring-0 active:text-black data-[active=true]:bg-transparent data-[active=true]:text-black [&>svg]:size-auto"
+							>
+								Create new conversation
+							</Button>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+		</>
+	);
+});
+
+export default function Header() {
 	return (
 		<SidebarMenu>
 			<SidebarMenuItem className="flex min-h-10 w-full items-center justify-between gap-2">
@@ -24,58 +85,7 @@ export default function Header() {
 				</SidebarMenuButton>
 
 				<div className="flex items-center justify-end">
-					<SidebarMenuButton
-						onClick={(e) => {
-							toggleSidebar();
-						}}
-						data-sidebar="trigger"
-						data-slot="sidebar-trigger"
-						tooltip={{ size: "sm", content: "Hide sidebar", side: "bottom" }}
-						isTooltipHidden={false}
-						className="group/sidebar-trigger hover:bg-sidebar-accent flex size-fit items-center justify-center overflow-hidden rounded-sm p-2 py-2.5 text-white opacity-100 transition-opacity duration-200 ease-in-out group-data-[collapsible=icon]:w-full group-data-[collapsible=icon]:rounded-md group-data-[collapsible=icon]:bg-transparent group-data-[collapsible=icon]:p-3 [&>svg]:size-auto"
-					>
-						<SidebarTriggerIcon width={15} height={15} stroke="white" className="right-10 rotate-180 rounded-xs bg-black/50 fill-none px-[1px]" />
-					</SidebarMenuButton>
-
-					<div className="hover:bg-sidebar-accent/65 flex h-9 w-fit items-center justify-center overflow-hidden rounded-xl">
-						<SidebarMenuButton
-							data-sidebar="create-chat"
-							data-slot="create-chat"
-							aria-label="Start New Chat"
-							tooltip={{ size: "sm", content: "Start New Chat", side: "bottom" }}
-							isTooltipHidden={false}
-							className="hover:bg-sidebar-accent flex size-full w-8 items-center justify-center py-2 text-white opacity-100 transition-opacity duration-200 ease-in-out [&>svg]:size-auto"
-						>
-							<PencilSquare width={25} height={25} className="fill-white stroke-black/50" />
-						</SidebarMenuButton>
-
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<SidebarMenuButton
-									data-sidebar="chat-dropdown"
-									data-slot="chat-dropdown"
-									aria-label="Actions Menu"
-									title="Open Actions Menu"
-									isTooltipHidden={true}
-									className="hover:bg-sidebar-accent flex size-full w-[1.5rem] items-center justify-center border-none py-2 text-white opacity-100 transition-opacity duration-200 ease-in-out focus-visible:ring-0 [&>svg]:size-auto"
-								>
-									<ChevronDown width={20} height={20} className="stroke-black/50" />
-								</SidebarMenuButton>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="start" side="bottom" className="border-none bg-white">
-								<DropdownMenuItem asChild className="hover:bg-sidebar-accent/50 cursor-pointer bg-transparent select-auto hover:text-black focus:bg-transparent">
-									<Button
-										aria-label="Create new conversation"
-										title="Create new conversation"
-										isTooltipHidden={true}
-										className="focus:bg-sidebar-accent/50 active:bg-sidebar-accent/50 relative flex items-center justify-start py-2 text-black opacity-100 shadow-none transition-opacity duration-200 ease-in-out focus:text-black focus-visible:ring-0 active:text-black data-[active=true]:bg-transparent data-[active=true]:text-black [&>svg]:size-auto"
-									>
-										Create new conversation
-									</Button>
-								</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</div>
+					<QuickActions />
 				</div>
 			</SidebarMenuItem>
 		</SidebarMenu>
--- resources/js/layouts/authenticated-layout.tsx
+++ resources/js/layouts/authenticated-layout.tsx
@@ -1,12 +1,12 @@
 import { usePage } from "@inertiajs/react";
-import React, { PropsWithChildren } from "react";
-
-import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { PropsWithChildren } from "react";
 
 import NavLink from "@/components/navigation-link";
 
 import { AppSidebar } from "@/components/sidebar/main";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, useSidebar } from "@/components/ui/sidebar";
+
+import { QuickActions } from "@/components/sidebar/header";
 
 const tabs = [
 	{ name: "Dashboard", href: "profile.dashboard" },
@@ -14,14 +14,20 @@ const tabs = [
 	{ name: "Admin Dashboard", href: "admin.dashboard", roles: ["admin"] },
 ];
 
-export default function AuthenticatedLayout({ children }: PropsWithChildren<{}>) {
+const PrivateHeader = () => {
 	const user = usePage().props.auth.user;
-	const isDesktop = useMediaQuery("(min-width: 768px)");
+	const { toggleSidebar, open, isMobile } = useSidebar();
 
 	return (
-		<React.Fragment>
-			<nav>
-				<div className="mx-auto max-w-7xl px-2">
+		<nav>
+			<header className="mx-auto flex w-full justify-items-normal px-2">
+				{!open && !isMobile && (
+					<div className="flex items-center justify-start">
+						<QuickActions />
+					</div>
+				)}
+
+				<div className="w-full max-w-7xl">
 					<div className="max-sm:no-scrollbar flex min-h-14 w-full items-center justify-start space-x-4 max-sm:max-w-full max-sm:overflow-x-auto max-sm:overflow-y-hidden md:m-2">
 						{tabs.map((tab) => {
 							if (tab.roles && !tab.roles.some((role) => user.roles.includes(role))) {
@@ -36,12 +42,19 @@ export default function AuthenticatedLayout({ children }: PropsWithChildren<{}>)
 						})}
 					</div>
 				</div>
-			</nav>
+			</header>
+		</nav>
+	);
+};
 
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset>{children}</SidebarInset>
-			</SidebarProvider>
-		</React.Fragment>
+export default function AuthenticatedLayout({ children }: PropsWithChildren<{}>) {
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<PrivateHeader />
+				{children}
+			</SidebarInset>
+		</SidebarProvider>
 	);
 }
