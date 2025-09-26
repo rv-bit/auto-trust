690da3e9a1cadb7b11970b3fa5df0ba3bcefd440
rv-bit <robertvilcu89@gmail.com>
2025-09-26 23:28:00 +0100

    chore: updated some common types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -4,19 +4,20 @@ export interface User {
 	id: number;
 	name: string;
 	email: string;
+	avatar?: string;
 	email_verified_at?: string;
 	roles: string[];
 	permissions: string[];
 }
 
+export type Methods = "get" | "post" | "put" | "patch" | "delete";
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-	flash: {
-		successPayment: boolean;
-	};
+	flash: {};
+	ziggy: Config & { location: string };
 	auth: {
 		user: User;
 	};
-	ziggy: Config & { location: string };
 	cookie: {
 		theme_state: string;
 		sidebar_state: boolean;
