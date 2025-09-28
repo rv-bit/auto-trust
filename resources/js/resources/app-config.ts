52bc4f13548fd58cefa2c60305987b1cac842611
rv-bit <robertvilcu89@gmail.com>
2025-09-28 14:51:37 +0100

    chore: changed constant to auto prefix from app name

--- resources/js/resources/app-config.ts
+++ resources/js/resources/app-config.ts
@@ -1,4 +1,4 @@
-export const COOKIE_PREFIX = "auto-trust";
+export const COOKIE_PREFIX = import.meta.env.VITE_APP_NAME.toLowerCase().replace(/\s+/g, "_");
 
 export const APP_NAME = import.meta.env.VITE_APP_NAME;
 export const APP_DESCRIPTION = import.meta.env.VITE_APP_DESCRIPTION;
