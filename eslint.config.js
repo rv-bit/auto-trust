21b1a2966907a4c46b25c97b0ff32ab2059b0508
rv-bit <robertvilcu89@gmail.com>
2025-10-19 13:25:31 +0100

    chore: eslint change configurations & fix timeout issue on websocket server

--- eslint.config.js
+++ eslint.config.js
@@ -7,38 +7,43 @@ import typescript from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    js.configs.recommended,
-    ...typescript.configs.recommended,
-    {
-        ...react.configs.flat.recommended,
-        ...react.configs.flat['jsx-runtime'], // Required for React 17+
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-            },
-        },
-        rules: {
-            'react/react-in-jsx-scope': 'off',
-            'react/prop-types': 'off',
-            'react/no-unescaped-entities': 'off',
-        },
-        settings: {
-            react: {
-                version: 'detect',
-            },
-        },
-    },
-    {
-        plugins: {
-            'react-hooks': reactHooks,
-        },
-        rules: {
-            'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
-        },
-    },
-    {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js'],
-    },
-    prettier, // Turn off all rules that might conflict with Prettier
+	js.configs.recommended,
+	{
+		...typescript.configs.recommended,
+		rules: {
+			"@typescript-eslint/no-unused-vars": "error",
+		},
+	},
+	{
+		...react.configs.flat.recommended,
+		...react.configs.flat["jsx-runtime"], // Required for React 17+
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+		rules: {
+			"react/react-in-jsx-scope": "off",
+			"react/prop-types": "off",
+			"react/no-unescaped-entities": "off",
+		},
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+	},
+	{
+		plugins: {
+			"react-hooks": reactHooks,
+		},
+		rules: {
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "warn",
+		},
+	},
+	{
+		ignores: ["vendor", "node_modules", "public", "bootstrap/ssr", "tailwind.config.js"],
+	},
+	prettier, // Turn off all rules that might conflict with Prettier
 ];
