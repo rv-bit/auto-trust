147e258e8651155ab71144ea5d8da4351bebcba5
rv-bit <robertvilcu89@gmail.com>
2025-10-20 21:52:59 +0100

    chore: add channels for broadcastings

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Resources\User\UserResource;
+
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('online', function (User $user) {
+    return $user ? new UserResource($user) : null;
+});
+
+Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, int $userId1, int $userId2) {
+    return $user->id === $userId1 || $user->id === $userId2 ? $user : null;
 });
