33e4b43908211d43cfc3eb0965405792c278fc24
rv-bit <robertvilcu89@gmail.com>
2025-10-20 21:56:45 +0100

    chore: update models with functions needed

--- app/Models/Chat/Conversation.php
+++ app/Models/Chat/Conversation.php
@@ -32,4 +32,34 @@ class Conversation extends Model
     {
         return $this->belongsTo(User::class, 'user_id2');
     }
+
+    public static function getAllConversations(User $user)
+    {
+        $users = User::getUsersExceptUser($user);
+        return $users->map(function (User $user) {
+            return $user->toConversationArray();
+        });
+    }
+
+    public static function updateConversationWithMessage($userId1, $userId2, $message) {
+        $conversation = Conversation::where(function($query) use($userId1, $userId2) {
+            $query->where('user_id1', $userId1)
+                ->where('user_id2', $userId2);
+        })->orWhere(function($query) use($userId1, $userId2) {
+            $query->where('user_id1', $userId2)
+                ->where('user_id2', $userId1);
+        })->first();
+
+        if ($conversation) {
+            $conversation->update([
+                'last_message_id' => $message->id,
+            ]);
+        } else {
+            Conversation::created([
+                'user_id1' => $userId1,
+                'user_id2' => $userId2,
+                'last_message_id' => $message->id,
+            ]);
+        }
+    }
 }
