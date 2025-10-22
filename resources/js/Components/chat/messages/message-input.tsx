e1e651f5eeb43b1126cd2430bdb72e0bb45d7489
rv-bit <robertvilcu89@gmail.com>
2025-10-22 22:24:00 +0100

    fix: type issues and others

--- resources/js/Components/chat/messages/message-input.tsx
+++ resources/js/Components/chat/messages/message-input.tsx
@@ -1,27 +1,19 @@
 import React from "@inertiajs/react";
-import { useRef, useEffect, useState, useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 import { Textarea } from "@/components/ui/textarea";
 
-export default function MessageInput({
-    value,
-    onSend,
-    onChange,
-}: {
-    value: string;
-    onSend: () => void;
-    onChange: (e: React.MouseEvent) => void;
-}) {
-    const input = useRef<HTMLTextAreaElement | null>(null);
+export default function MessageInput({ value, onSend, onChange }: { value: string; onSend: () => void; onChange: (e: React.MouseEvent) => void }) {
+	const input = useRef<HTMLTextAreaElement | null>(null);
 
-    const onHandleSubmit = (e: React.KeyboardEventHandler<HTMLTextAreaElement>) => {
+	const onHandleSubmit = (e: React.KeyboardEventHandler<HTMLTextAreaElement>) => {
 		if (e.key === "Enter" && !e.shiftKey) {
 			e.preventDefault();
 			onSend();
 		}
 	};
 
-    const onHandleOnChangeEvent = (e: React.ChangeEventHandler<HTMLTextAreaElement> | undefined) => {
+	const onHandleOnChangeEvent = (e: React.ChangeEventHandler<HTMLTextAreaElement> | undefined) => {
 		setTimeout(() => {
 			adjustHeight();
 		}, 10);
@@ -29,32 +21,32 @@ export default function MessageInput({
 		onChange(e);
 	};
 
-    const adjustHeight = useCallback(() => {
-        const timeout = setTimeout(() => {
-            if (!input.current) return;
-            input.current.style.height = 'auto';
-            input.current.style.height = input.current.scrollHeight + 1 + "px";
-        }, 100)
+	const adjustHeight = useCallback(() => {
+		const timeout = setTimeout(() => {
+			if (!input.current) return;
+			input.current.style.height = "auto";
+			input.current.style.height = input.current.scrollHeight + 1 + "px";
+		}, 100);
 
-        return () => {
-            clearTimeout(timeout)
-        }
-    }, [])
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, []);
 
-    useEffect(() => {
-        adjustHeight();
-        return () => {}
-    }, [input])
+	useEffect(() => {
+		adjustHeight();
+		return () => {};
+	}, [input]);
 
-    return (
-        <Textarea 
-            ref={input}
-            value={value}
-            rows={1}
-            placeholder="Type a message"
-            onKeyDown={onHandleSubmit}
-            onChange={(e: React.MouseEvent) => onHandleOnChangeEvent(e)}
-            className="max-h-40 overflow-y-auto resize-none rounded-r-none w-full"
-        />
-    )
+	return (
+		<Textarea
+			ref={input}
+			value={value}
+			rows={1}
+			placeholder="Type a message"
+			onKeyDown={onHandleSubmit}
+			onChange={(e: React.MouseEvent) => onHandleOnChangeEvent(e)}
+			className="max-h-40 w-full resize-none overflow-y-auto rounded-r-none"
+		/>
+	);
 }
