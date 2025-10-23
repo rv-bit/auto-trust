d4de0d8f6bdf9ac13a71428622cdd0164103fdcb
rv-bit <robertvilcu89@gmail.com>
2025-10-23 22:22:00 +0100

    fix: formatting

--- resources/js/lib/helpers.ts
+++ resources/js/lib/helpers.ts
@@ -1,49 +1,58 @@
 export const formatMessageDateLong = (date: Date | string) => {
-    const now = new Date();
-    const inputDate = new Date(date);
+	const now = new Date();
+	const inputDate = new Date(date);
 
-    if (isToday(inputDate)) {
-        return inputDate.toLocaleDateString([], {
-            hour: "2-digit",
-            minute: "2-digit"
-        })
-    } else if (isYesterday(inputDate)) {
-        const dateString = inputDate.toLocaleDateString([], {
-            hour: "2-digit",
-            minute: "2-digit"
-        })
+	if (isToday(inputDate)) {
+		const dateString = inputDate.toLocaleTimeString([], {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
 
-        return `Yesterday ${dateString}`
-    } else if (inputDate.getFullYear() === now.getFullYear()) {
-        return inputDate.toLocaleDateString([], {
-            day: "2-digit",
-            month: "short"
-        })
-    } else {
-        return inputDate.toLocaleDateString()
-    }
-}
+		return `${dateString}`;
+	} else if (isYesterday(inputDate)) {
+		const dateString = inputDate.toLocaleTimeString([], {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+
+		return `Yesterday at ${dateString}`;
+	} else if (inputDate.getFullYear() === now.getFullYear()) {
+		return inputDate.toLocaleDateString([], {
+			day: "2-digit",
+			month: "short",
+		});
+	} else {
+		return inputDate.toLocaleDateString();
+	}
+};
 
 export const formatMessageDateShort = (date: Date | string) => {
-    const now = new Date();
-    const inputDate = new Date(date);
+	const now = new Date();
+	const inputDate = new Date(date);
 
-    if (isToday(inputDate)) {
-        return inputDate.toLocaleDateString([], {
-            hour: "2-digit",
-            minute: "2-digit"
-        })
-    } else if (isYesterday(inputDate)) {
-        return `Yesterday`
-    } else if (inputDate.getFullYear() === now.getFullYear()) {
-        return inputDate.toLocaleDateString([], {
-            day: "2-digit",
-            month: "short"
-        })
-    } else {
-        return inputDate.toLocaleDateString()
-    }
-}
+	if (isToday(inputDate)) {
+		const dateString = inputDate.toLocaleTimeString([], {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+
+		return `${dateString}`;
+	} else if (isYesterday(inputDate)) {
+		const dateString = inputDate.toLocaleTimeString([], {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+
+		return `Yesterday at ${dateString}`;
+	} else if (inputDate.getFullYear() === now.getFullYear()) {
+		return inputDate.toLocaleDateString([], {
+			day: "2-digit",
+			month: "short",
+		});
+	} else {
+		return inputDate.toLocaleDateString();
+	}
+};
 
 const isToday = (date: Date) => {
     const today = new Date();
