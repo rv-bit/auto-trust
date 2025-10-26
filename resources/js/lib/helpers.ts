98c1dd1bae744816533819f67c9292e3fbb7421d
rv-bit <robertvilcu89@gmail.com>
2025-10-26 15:28:41 +0000

    chore: refactor helper functions

--- resources/js/lib/helpers.ts
+++ resources/js/lib/helpers.ts
@@ -1,75 +1,40 @@
-export const formatMessageDateLong = (date: Date | string) => {
+export const formatMessageDate = (date: Date | string) => {
 	const now = new Date();
 	const inputDate = new Date(date);
 
 	if (isToday(inputDate)) {
-		const dateString = inputDate.toLocaleTimeString([], {
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-
-		return `${dateString}`;
+		return "Today";
 	} else if (isYesterday(inputDate)) {
-		const dateString = inputDate.toLocaleTimeString([], {
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-
-		return `Yesterday at ${dateString}`;
-	} else if (inputDate.getFullYear() === now.getFullYear()) {
-		return inputDate.toLocaleDateString([], {
-			day: "2-digit",
-			month: "short",
+		return "Yesterday";
+	} else if (isThisWeek(inputDate)) {
+		return inputDate.toLocaleDateString("en-GB", {
+			weekday: "long",
 		});
 	} else {
-		return inputDate.toLocaleDateString();
+		return inputDate.toLocaleDateString("en-GB", {
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric",
+		});
 	}
 };
 
-export const formatMessageDateShort = (date: Date | string) => {
-	const now = new Date();
-	const inputDate = new Date(date);
-
-	if (isToday(inputDate)) {
-		const dateString = inputDate.toLocaleTimeString([], {
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-
-		return `${dateString}`;
-	} else if (isYesterday(inputDate)) {
-		const dateString = inputDate.toLocaleTimeString([], {
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-
-		return `Yesterday at ${dateString}`;
-	} else if (inputDate.getFullYear() === now.getFullYear()) {
-		return inputDate.toLocaleDateString([], {
-			day: "2-digit",
-			month: "short",
-		});
-	} else {
-		return inputDate.toLocaleDateString();
-	}
+export const isToday = (date: Date) => {
+	const today = new Date();
+	return date.getDate() === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear();
 };
 
-const isToday = (date: Date) => {
-    const today = new Date();
-    return (
-        date.getDate() === today.getDate() &&
-        date.getMonth() === today.getMonth() &&
-        date.getFullYear() === today.getFullYear()
-    );
-}
+export const isYesterday = (date: Date) => {
+	const yesterday = new Date();
+	yesterday.setDate(yesterday.getDate() - 1);
 
-const isYesterday = (date: Date) => {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
+	return date.getDate() === yesterday.getDate() && date.getMonth() === yesterday.getMonth() && date.getFullYear() === yesterday.getFullYear();
+};
 
-    return (
-        date.getDate() === yesterday.getDate() &&
-        date.getMonth() === yesterday.getMonth() &&
-        date.getFullYear() === yesterday.getFullYear()
-    );
-}
+export const isThisWeek = (date: Date) => {
+	const today = new Date();
+	const firstDayOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
+	const lastDayOfWeek = new Date(today.setDate(firstDayOfWeek.getDate() + 6));
+
+	return date >= firstDayOfWeek && date <= lastDayOfWeek;
+};
