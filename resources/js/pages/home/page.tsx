7bc0a7b5cf4fa697a6167ffa59320e0453a0e230
rv-bit <robertvilcu89@gmail.com>
2025-10-28 22:05:02 +0000

    chore: moved files

--- resources/js/pages/home/page.tsx
+++ resources/js/pages/home/page.tsx
@@ -1,35 +1,26 @@
-import { Head } from "@inertiajs/react";
+import { Head, usePage } from "@inertiajs/react";
 import React from "react";
 
-import { Footer } from "@/components/application/app-footer";
-import { NavHeader } from "@/components/application/app-header";
-import { AppShell } from "@/components/application/app-shell";
+import { SharedData } from "@/types";
+
+import AppLayout from "@/layouts/app/app-shell-layout";
 
 export default function Welcome() {
-	// const { auth } = usePage<SharedData>().props;
+	const { auth } = usePage<SharedData>().props;
 
 	// trick the theme to force white mode under all divs
 	React.useEffect(() => {
-		document.body.classList.add("landing");
+		document.body.classList.add("overwrite-white-mode");
 		return () => {
-			document.body.classList.remove("landing");
+			document.body.classList.remove("overwrite-white-mode");
 		};
 	}, []);
 
 	return (
-		<AppShell variant="header" className="relative flex justify-start text-black">
+		<AppLayout withFooter>
 			<Head title="Welcome"></Head>
 
-			<NavHeader />
-
-			<main
-				style={{
-					minHeight: "100%",
-					width: "100%",
-					paddingTop: "var(--header-h)",
-				}}
-				className="z-0 flex flex-col items-center justify-start px-6 pt-4 lg:px-8 lg:pt-4"
-			>
+			<div className="flex flex-col items-center justify-start px-6 lg:px-8">
 				<h1>Hi</h1>
 				<h1>Hi</h1>
 				<h1>Hi</h1>
@@ -135,9 +126,7 @@ export default function Welcome() {
 				<h1>Hi</h1>
 				<h1>Hi</h1>
 				<h1>Hi</h1>
-			</main>
-
-			<Footer className="border-border flex h-fit w-full flex-col items-center justify-end border-t bg-transparent px-6 lg:px-8" />
-		</AppShell>
+			</div>
+		</AppLayout>
 	);
 }
