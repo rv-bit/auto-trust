db073d088a91cb74925b6bc9c202ca9f3d12e736
rv-bit <robertvilcu89@gmail.com>
2025-11-01 15:27:52 +0000

    chore: refactor model to make it more understandable & add more fillable

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -5,35 +5,54 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Str;
 
 class Notification extends Model
 {
     use HasFactory;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
+
     protected $fillable = [
+        'id',
         'type',
+        'notifiable_type',
+        'notifiable_id',
         'data',
-        'seen_at',
+        'read_at',
     ];
 
     protected $casts = [
         'data' => 'array',
-        'seen_at' => 'datetime',
+        'read_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
 
     public function notifiable(): MorphTo
     {
         return $this->morphTo();
     }
 
-    public function scopeUnseen($query)
+    public function scopeUnread($query)
     {
-        return $query->whereNull('seen_at');
+        return $query->whereNull('read_at');
     }
 
-    public function markAsSeen(): bool
+    public function markAsRead(): bool
     {
-        $this->seen_at = now();
+        $this->read_at = now();
         return $this->save();
     }
 }
+
