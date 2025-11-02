0ae8db112bf77f6c09fdc38aed9449457af2e5f0
rv-bit <robertvilcu89@gmail.com>
2025-11-02 19:29:56 +0000

    fix: route roles, needed configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,13 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         ]);
+
+        // Register Spatie Permission middleware aliases
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
